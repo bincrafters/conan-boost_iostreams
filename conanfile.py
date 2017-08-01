5be5e4d2b26d3ecbe178f00ed5531cfc347f24f9
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:19:38 -0400

    added package info header-only flag

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,36 @@ class BoostIostreamsConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/iostreams"
-    description = "For a description of this library, please visit http://boost.org/iostreams "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "iostreams"
+    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Bind/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Detail/1.64.0@bincrafters/testing", \
+                      "Boost.Function/1.64.0@bincrafters/testing", \
+                      "Boost.Integer/1.64.0@bincrafters/testing", \
+                      "Boost.Mpl/1.64.0@bincrafters/testing", \
+                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
+                      "Boost.Random/1.64.0@bincrafters/testing", \
+                      "Boost.Range/1.64.0@bincrafters/testing", \
+                      "Boost.Regex/1.64.0@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
+                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
+                      "Boost.Utility/1.64.0@bincrafters/testing"
 
+                      #assert1 bind3 config0 core2 detail5 function5 integer3 mpl5 preprocessor0 random9 range7 regex6 smart_ptr4 static_assert1 throw_exception2 type_traits3 utility5
+                      
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()

f45e5b69b26da762b36b8802acf1b2c4255406f0
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:45:14 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,7 @@ class BoostIostreamsConan(ConanFile):
     version = "1.64.0"
     generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-iostreams"
     source_url = "https://github.com/boostorg/iostreams"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -33,7 +34,7 @@ class BoostIostreamsConan(ConanFile):
                       
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def build(self):

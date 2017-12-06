83c3cd16af1b420acd9a3d283f9c37775e6169ec
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:46 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,12 @@ from conans import ConanFile, tools
 class BoostIostreamsConan(ConanFile):
     name = "Boost.Iostreams"
     version = "1.65.1"
-    url = "https://github.com/bincrafters/conan-boost-iostreams"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
-    license = "www.boost.org/users/license.html"
 
-    settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], 'use_zlib': [True, False], 'use_bzip2': [True, False]}
     default_options = "shared=False", "use_zlib=True", "use_bzip2=True"
 
     requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
         "Boost.Assert/1.65.1@bincrafters/testing", \
         "Boost.Bind/1.65.1@bincrafters/testing", \
         "Boost.Config/1.65.1@bincrafters/testing", \
@@ -49,20 +46,19 @@ class BoostIostreamsConan(ConanFile):
 
     # BEGIN
 
+    url = "https://github.com/bincrafters/conan-boost-iostreams"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
     generators = "boost"
+    settings = "os", "arch", "compiler", "build_type"
 
-    def package_id(self):
-        if self.is_header_only:
-            self.info.header_only()
-
-    # pylint: disable=unused-import
     @property
     def env(self):
         try:
             with tools.pythonpath(super(self.__class__, self)):
-                import boostgenerator # pylint: disable=F0401
+                import boostgenerator  # pylint: disable=F0401
                 boostgenerator.BoostConanFile(self)
         except:
             pass

2fc2e85f241550035be0fe7a94b1db4597f03319
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:32 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostIostreamsConan(ConanFile):
     name = "boost_iostreams"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_iostreams"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["iostreams"]
     is_header_only = False
 

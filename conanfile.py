50d4aef23f49e102adf298e4f3a5272ffa4a4a76
Rene Rivera <grafikrobot@gmail.com>
2018-01-29 09:53:49 -0600

    Merge.

--- conanfile.py
+++ conanfile.py
@@ -17,24 +17,24 @@ class BoostIostreamsConan(ConanFile):
     default_options = "shared=False", "use_zlib=False", "use_bzip2=False", "use_lzma=False"
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/stable",
-        "boost_assert/1.66.0@bincrafters/stable",
-        "boost_bind/1.66.0@bincrafters/stable",
-        "boost_config/1.66.0@bincrafters/stable",
-        "boost_core/1.66.0@bincrafters/stable",
-        "boost_detail/1.66.0@bincrafters/stable",
-        "boost_function/1.66.0@bincrafters/stable",
-        "boost_integer/1.66.0@bincrafters/stable",
-        "boost_mpl/1.66.0@bincrafters/stable",
-        "boost_preprocessor/1.66.0@bincrafters/stable",
-        "boost_random/1.66.0@bincrafters/stable",
-        "boost_range/1.66.0@bincrafters/stable",
-        "boost_regex/1.66.0@bincrafters/stable",
-        "boost_smart_ptr/1.66.0@bincrafters/stable",
-        "boost_static_assert/1.66.0@bincrafters/stable",
-        "boost_throw_exception/1.66.0@bincrafters/stable",
-        "boost_type_traits/1.66.0@bincrafters/stable",
-        "boost_utility/1.66.0@bincrafters/stable"
+        "boost_package_tools/1.66.0@bincrafters/testing",
+        "boost_assert/1.66.0@bincrafters/testing",
+        "boost_bind/1.66.0@bincrafters/testing",
+        "boost_config/1.66.0@bincrafters/testing",
+        "boost_core/1.66.0@bincrafters/testing",
+        "boost_detail/1.66.0@bincrafters/testing",
+        "boost_function/1.66.0@bincrafters/testing",
+        "boost_integer/1.66.0@bincrafters/testing",
+        "boost_mpl/1.66.0@bincrafters/testing",
+        "boost_preprocessor/1.66.0@bincrafters/testing",
+        "boost_random/1.66.0@bincrafters/testing",
+        "boost_range/1.66.0@bincrafters/testing",
+        "boost_regex/1.66.0@bincrafters/testing",
+        "boost_smart_ptr/1.66.0@bincrafters/testing",
+        "boost_static_assert/1.66.0@bincrafters/testing",
+        "boost_throw_exception/1.66.0@bincrafters/testing",
+        "boost_type_traits/1.66.0@bincrafters/testing",
+        "boost_utility/1.66.0@bincrafters/testing"
     )
     
     def requirements(self):
@@ -65,7 +65,7 @@ class BoostIostreamsConan(ConanFile):
     short_paths = True
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
-    build_requires = "boost_generator/1.66.0@bincrafters/stable"
+    build_requires = "boost_generator/1.66.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()

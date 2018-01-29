4fc326b502c6916b30bc311fd360e7ba50208d55
Jerry W <jerrywiltse@gmail.com>
2018-01-29 16:53:25 -0500

    add full_version_mode to all boost deps

--- conanfile.py
+++ conanfile.py
@@ -58,6 +58,12 @@ class BoostIostreamsConan(ConanFile):
             return "define=LZMA_API_STATIC"
         return ""
       
+    def package_id_additional(self):
+        boost_deps_only = [dep_name for dep_name in self.info.requires.pkg_names if dep_name.startswith("boost_")]
+
+        for dep_name in boost_deps_only:
+            self.info.requires[dep_name].full_version_mode()
+
     # BEGIN
 
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"

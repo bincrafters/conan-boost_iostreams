509b0d33e197b075e082b18f87ed901e7ec17c0b
Jerry W <jerrywiltse@gmail.com>
2019-01-22 21:57:17 -0500

    use clang 5.0 for header only, add clang 7 to others, implement versions in recipes, conan no longer allows inheriting version, refresh travis badge, use .ci dir, python 3.6 in appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.67.0@bincrafters/testing")
 
 class BoostIostreamsConan(base.BoostBaseConan):
     name = "boost_iostreams"
+    version = "1.67.0"
     url = "https://github.com/bincrafters/conan-boost_iostreams"
     lib_short_names = ["iostreams"]
     options = {"shared": [True, False], 'use_zlib': [True, False], 'use_bzip2': [True, False], 'use_lzma': [True, False]}

360392b3f369bb94cabd8a94cf751de76f5038f3
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:05 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,20 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostIostreamsConan(base.BoostBaseConan):
     name = "boost_iostreams"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_iostreams"
-    lib_short_names = ["iostreams"]
-    cycle_group = "boost_cycle_group_c"
-    options = {
-        "shared": [True, False],
-        "use_bzip2": [True, False],
-        "use_lzma": [True, False],
-        "use_zlib": [True, False],
-        "use_zstd": [True, False]
-    }
-    default_options = "shared=False", "use_bzip2=True", "use_lzma=True", "use_zlib=True", "use_zstd=True"
-    b2_requires = ["boost_cycle_group_c"]
+    version = "1.70.0"
